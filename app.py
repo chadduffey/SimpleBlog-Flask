e2e10489feafcf50c49cc581419296d196ba181c
Chad Duffey <chadduffey@hotmail.com>
2015-05-03 18:08:30 +1000

    added flash messages

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form 
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Something tricky XRTY ^%&ERT *&^$%'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	user_agent = request.headers.get('User-Agent')
-	return '<h1>Hello</h1><p>Your browser is %s</p>' % user_agent
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
-	return render_template('user.html', name=name)
+	form = NameForm()
+	return render_template('user.html', name=name, current_time=datetime.utcnow(), form=form)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'), 500
+
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[Required()])
+	submit = SubmitField('Submit')
 
 if __name__ == '__main__':
 	app.run(debug=True)
